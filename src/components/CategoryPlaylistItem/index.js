a74b39b7292246508066f5d4aa1cbf62961c24f5
Prem324 <prem324r@gamil.com>
2024-10-23 17:05:17 +0000

    Updated Code

--- src/components/CategoryPlaylistItem/index.js
+++ src/components/CategoryPlaylistItem/index.js
@@ -1,20 +1,23 @@
+import {Link} from 'react-router-dom'
 import './index.css'
 
 const CategoryPlaylistItem = props => {
   const {categoryDetails} = props
-  const {name, images, tracks} = categoryDetails
+  const {id, name, images, tracks} = categoryDetails
   const imageUrl = images[0]
   return (
     <li className="category-item">
-      <div className="category-content">
-        <img
-          className="category-item-image"
-          src={imageUrl.url}
-          alt="category"
-        />
-        <p className="category-item-name">{name}</p>
-        <p className="category-item-tracks-count">{`${tracks.total} Tracks`}</p>
-      </div>
+      <Link to={`playlists-details/${id}`}>
+        <div className="category-content">
+          <img
+            className="category-item-image"
+            src={imageUrl.url}
+            alt="category"
+          />
+          <p className="category-item-name">{name}</p>
+          <p className="category-item-tracks-count">{`${tracks.total} Tracks`}</p>
+        </div>
+      </Link>
     </li>
   )
 }
